235524b204c894c42ca7c0952913ee235182164e
joey_wan <67672483+B200161B@users.noreply.github.com>
2020-11-29 22:31:39 +0800

    Update insert_book.php

--- insert_book.php
+++ insert_book.php
@@ -9,16 +9,25 @@
     <h1>Book Entry Results</h1>
     <?php
     // TODO 1: Create short variable names.
-
-
+    
+    if($conn->connect_error) die("Fatal Error");
+    $isbn = $_POST['isbn'];
+    $author = $_POST['author'];
+    $title = $_POST['title'];
+    $price = $_POST['price'];
+    
+    //insert into statement
+   
+    if(!$result) die("Database access failed");
     // TODO 2: Check and filter data coming from the user.
-
+    //check the data whether is empty or not
+    if(isset($ibsn))
 
     // TODO 3: Setup a connection to the appropriate database.
-
-
+    $conn=new mysqli($hn,$un,$pw,$db);
     // TODO 4: Query the database.
-
+    $query="INSERT INTO catologs VALUES($isbn,$author,$title,$price)";
+    $result=$conn->query($query);
 
     // TODO 5: Display the feedback back to user.
 
